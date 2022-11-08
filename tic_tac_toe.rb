2f03199587c31e766228d3dc77730339a755afa1
danierokon <danierokon@gmail.com>
2022-11-08 19:49:06 +0800

    add win/draw condition check and conclusion

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,23 +2,22 @@
 class Board
   attr_reader :cell, :board
 
+  WINNING_COMBO = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   def initialize
     @cell = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @board = [[@cell[0], @cell[1], @cell[2]], [@cell[3], @cell[4], @cell[5]], [@cell[6], @cell[7], @cell[8]]]
   end
 
-  def update_board(board = @board)
+  def update_board(_board = @cell)
     puts "\n
-           #{board[0][0]} | #{board[0][1]} | #{board[0][2]} \n
+           #{cell[0]} | #{cell[1]} | #{cell[2]} \n
           -----------
-           #{board[1][0]} | #{board[1][1]} | #{board[1][2]} \n
+           #{cell[3]} | #{cell[4]} | #{cell[5]} \n
           -----------
-           #{board[2][0]} | #{board[2][1]} | #{board[2][2]}"
+           #{cell[6]} | #{cell[7]} | #{cell[8]}"
   end
 
   def cell_update(number, marker)
     @cell[number - 1] = marker
-    @board = [[@cell[0], @cell[1], @cell[2]], [@cell[3], @cell[4], @cell[5]], [@cell[6], @cell[7], @cell[8]]]
   end
 end
 
@@ -29,17 +28,18 @@ class Game
   def initialize
     @firstplayer_turn = nil
     @current_board = Board.new
+    @winner = nil
   end
 
   def play
-    # menu
-    menu
     # initialize game?
     start_game
     # gaming
     gaming
     # result
+    declare_result
     # replay?
+    menu
   end
 
   def menu
@@ -49,6 +49,7 @@ class Game
       input = gets.chomp.downcase
       if input == 'y'
         stuck_in_menu = false
+        play
       elsif input == 'n'
         stuck_in_menu = false
         puts 'Alright, see you around!'
@@ -75,7 +76,6 @@ class Game
     puts 'See the board below? The position is labelld with numbers as shown.'
     puts 'When inputting your next move, use the numbers to refer to the position of your choice!(1-9)'
     @current_board.update_board
-    puts @firstplayer_turn
     until match_over == true
       case @firstplayer_turn
       when true
@@ -86,10 +86,14 @@ class Game
         @current_board.cell_update(x_marker, 'X')
         @firstplayer_turn = false
       when false
-        puts 'yo'
+        puts 'Player 2, please choose a valid spot to place your marker (O).'
+        o_marker = gets.chomp.to_i
+        o_marker = input_check(o_marker)
+        @current_board.cell_update(o_marker, 'O')
         @firstplayer_turn = true
       end
-      next
+      @current_board.update_board(@current_board.cell)
+      match_over = true if check_board_full? || winner_exist?
     end
   end
 
@@ -97,26 +101,46 @@ class Game
     until (1..9).include?(input) && valid_space_check(input)
       puts 'Please enter a valid input!(1-9)'
       puts 'Makes sure the spot is not occupied.'
-      puts input
       input = gets.chomp.to_i
     end
-    puts "valid input is #{input}"
     input
   end
 
   def valid_space_check(input)
     @current_board.cell.each_with_index do |cell, index|
       return true if index == input - 1 && (cell == index - 1)
+      return false if index == input - 1 && %w[X O].include?(cell)
     end
   end
 
-  def three_equal?(a, b, c)
-    # check if the three cells are equal, determine winner base on X or O
+  def winner_exist?
+    # check if any winning combo exist
+    Board::WINNING_COMBO.any? do |combo|
+      [@current_board.cell[combo[0]],
+       @current_board.cell[combo[1]],
+       @current_board.cell[combo[2]]].uniq.length == 1
+    end
   end
 
-  def check_winner
-    # equal
+  def declare_result
+    if winner_exist?
+      @winner = firstplayer_turn == true ? 'O' : 'X'
+      case @winner
+      when 'X'
+        puts 'Congratualtions, player 1! You Won!'
+      when 'O'
+        puts 'Congratulations, player 2! You Won!'
+      end
+    elsif check_board_full?
+      puts "It's a TIE! The board is full and no more move can be made!"
+    end
+    puts "Press enter to return to menu."
+    gets
+  end
+
+  def check_board_full?
+    return true unless @current_board.cell.any? { |cell| cell.is_a? Numeric }
   end
 end
 new_game = Game.new
-new_game.play
+new_game.menu
